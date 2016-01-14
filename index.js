a1b5121d62908a5dba81b50106de1190fb56e0ad
Avi Haiat <thaiat@ipelia.fr>
2016-01-14 16:53:49 +0100

    feat(app): Implement ngux loader

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
-/*
-	MIT License http://www.opensource.org/licenses/mit-license.php
-	Author Tobias Koppers @sokra
-*/
-var loaderUtils = require("loader-utils");
+'use strict';
+var path = require('path');
+var fs = require('fs');
 
 module.exports = function(content) {
-	this.cacheable && this.cacheable();
-	if(!this.emitFile) throw new Error("emitFile is required from module system");
-	var query = loaderUtils.parseQuery(this.query);
-	var url = loaderUtils.interpolateName(this, query.name || "[hash].[ext]", {
-		context: query.context || this.options.context,
-		content: content,
-		regExp: query.regExp
-	});
-	this.emitFile(url, content);
-	return "module.exports = __webpack_public_path__ + " + JSON.stringify(url) + ";";
-}
-module.exports.raw = true;
+    this.cacheable();
+    var callback = this.async();
+    var folder = path.resolve(__dirname, 'bin', 'ngux', 'NGUX.exe');
+    var resourcePath = this.resourcePath;
+    var exec = require('child_process').exec;
+    exec('mono ' + folder + ' ' + resourcePath + ' ' + path.dirname(this.resourcePath), function(err, stdout, stderr) {
+        if (err) {
+            callback(err);
+            return;
+        }
+        var htmlPath = resourcePath.replace('.ngux', '.html');
+        fs.readFile(htmlPath, 'utf8', function(err, content) {
+            callback(null, content);
+        });
+    });
+};
