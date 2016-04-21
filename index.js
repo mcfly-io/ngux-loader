8ffed788a3d58819ac95bae86b24ea8f5b4791a6
Joshua Estrin Skrzypek <jskrzypek@gmail.com>
2016-04-21 20:11:39 +0300

    feat(app): Refactor to handle .html, .js, & .g.ux in loader

--- index.js
+++ index.js
@@ -22,25 +22,40 @@ module.exports = function(content) {
     var exec = require('child_process').exec;
     var outputDir = path.dirname(resourcePath);
     var loaderOptions = loaderUtils.parseQuery(this.query);
-    if (loaderOptions.subdir) {
-        outputDir = path.join(outputDir, loaderOptions.subdir);
-    }
-    if (this.options.outputDir) {
-        outputDir = this.options.outputDir;
+
+    // if (loaderOptions.subdir) {
+    //     outputDir = path.join(outputDir, loaderOptions.subdir);
+    // }
+
+    // make sure that the subdir is a truthy so we can remove it 
+    outputDir = path.join(outputDir, this.options.subdir ||  loaderOptions.subdir || 'ngux'); 
+    if (this.options.outputDir || loaderOptions.outputDir) {
+        outputDir = this.options.outputDir || loaderOptions.outputDir;
     }
     exec((os.platform() === 'win32' ? '' : 'mono ') + folder + ' ' + resourcePath + ' ' + outputDir, function(err, stdout, stderr) {
         if (err) {
             callback(err);
             return;
         }
-        var htmlPath = getOutputPath(resourcePath, outputDir, '.html');
-        //var jsPath = getOutputPath(resourcePath, outputDir, '.js');
-
-        // causes infinite loop so i deactivated those 2 lines
+        //causes infinite loop so is deactivated those 2 lines
         //this.addDependency(path.resolve(htmlPath));
         //this.addDependency(path.resolve(jsPath));
+
+        var htmlPath = getOutputPath(resourcePath, outputDir, '.html');
+        var jsPath = getOutputPath(resourcePath, outputDir, '.js');
+        var uxPath = getOutputPath(resourcePath, outputDir, '.g.ux');
+        var uxName = path.basename(uxPath);
+        var resourceDir = path.dirname(resourcePath);
+        var uxContext = path.relative(this._compilation.options.context, path.join(resourceDir, uxName));
         fs.readFile(htmlPath, 'utf-8', function(err, htmlContent) {
-            callback(null, htmlContent);
-        });
+            fs.readFile(jsPath, 'utf-8', function(err, jsContent) {
+                fs.readFile(uxPath, null, function(err, uxContent) {
+                    this.emitFile(uxContext, uxContent);
+                    fs.rmdir(outputDir, null, function(err) {
+                        callback(null, jsContent + '; module.exports = ' + JSON.stringify(htmlContent) + ';');
+                    }.bind(this));
+                }.bind(this));
+            }.bind(this));
+        }.bind(this));
     }.bind(this));
 };
